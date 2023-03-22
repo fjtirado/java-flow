a9662ebfd8d31ff7f46d98bec3a9c3ff97707d85
Francisco Javier Tirado Sarti <ftirados@redhat.com>
2023-03-22 12:39:27 +0100

    Adding Java function call

--- java-flow-example/src/main/java/org/kie/kogito/serverless/workflow/examples/FullExample.java
+++ java-flow-example/src/main/java/org/kie/kogito/serverless/workflow/examples/FullExample.java
@@ -1,29 +1,37 @@
 package org.kie.kogito.serverless.workflow.examples;
 
-import java.util.Map;
-
-import org.kie.kogito.process.Process;
-import org.kie.kogito.serverless.workflow.executor.StaticWorkflowApplication;
-import org.kie.kogito.serverless.workflow.fluent.FunctionBuilder.HttpMethod;
-import org.kie.kogito.serverless.workflow.models.JsonNodeModel;
-
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
-import io.serverlessworkflow.api.Workflow;
-
 import static org.kie.kogito.serverless.workflow.fluent.ActionBuilder.call;
 import static org.kie.kogito.serverless.workflow.fluent.ActionBuilder.subprocess;
+import static org.kie.kogito.serverless.workflow.fluent.FunctionBuilder.java;
 import static org.kie.kogito.serverless.workflow.fluent.FunctionBuilder.rest;
 import static org.kie.kogito.serverless.workflow.fluent.StateBuilder.operation;
 import static org.kie.kogito.serverless.workflow.fluent.StateBuilder.parallel;
 import static org.kie.kogito.serverless.workflow.fluent.WorkflowBuilder.objectNode;
 import static org.kie.kogito.serverless.workflow.fluent.WorkflowBuilder.workflow;
 
+import java.util.Collections;
+import java.util.Map;
+
+import org.kie.kogito.process.Process;
+import org.kie.kogito.serverless.workflow.executor.StaticWorkflowApplication;
+import org.kie.kogito.serverless.workflow.fluent.FunctionBuilder.HttpMethod;
+import org.kie.kogito.serverless.workflow.models.JsonNodeModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+
+import io.serverlessworkflow.api.Workflow;
+
 public class FullExample {
+
+    private static final Logger logger = LoggerFactory.getLogger(FullExample.class);
 
     private static final String AGE_FUNCTION = "GET_AGE";
     private static final String PERSON_COUNTRY_ID_FUNCTION = "GET_COUNTRY_ID";
     private static final String COUNTRY_ID_NAME_FUNCTION = "GET_COUNTRY_NAME";
+    private static final String PRINT_AGE = "PRINT_AGE";
     private static final String GENDER_FUNCTION = "GET_GENDER";
     private static final String UNIVERSITY_FUNCTION = "GET_UNIVERSITY";
     private static final String WEATHER_FUNCTION = "GET_WEATHER";
@@ -41,12 +49,14 @@ public class FullExample {
 
             Workflow flow = workflow("FullExample").function(rest(AGE_FUNCTION, HttpMethod.get, "https://api.agify.io/?name={name}"))
                     .constant("apiKey", "2482c1d33308a7cffedff5764e9ef203")
+                    .function(java(PRINT_AGE, FullExample::logAge))
                     .function(rest(GENDER_FUNCTION, HttpMethod.get, "https://api.genderize.io/?name={name}"))
                     .function(rest(UNIVERSITY_FUNCTION, HttpMethod.get, "http://universities.hipolabs.com/search?country={country}"))
                     .function(rest(WEATHER_FUNCTION, HttpMethod.get, "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={appid}"))
                     .start(parallel().newBranch().action(call(AGE_FUNCTION, nameArgs).resultFilter("{age}")).endBranch()
                             .newBranch().action(subprocess(subprocess)).endBranch()
                             .newBranch().action(call(GENDER_FUNCTION, nameArgs).resultFilter("{gender}")).endBranch())
+                    .next(operation().action(call(PRINT_AGE, new TextNode(".age"))))
                     .when(".age<50").end(operation().action(call(UNIVERSITY_FUNCTION, objectNode().put("country", ".country.name")).resultFilter(".[].name").outputFilter(".universities")))
                     .or().end(operation().action(call(WEATHER_FUNCTION, objectNode().put("lat", ".country.latitude").put("lon", ".country.longitude").put("appid", "$CONST.apiKey"))
                             .resultFilter("{weather:.main}")))
@@ -54,9 +64,14 @@ public class FullExample {
             // create a reusable process for several executions
             Process<JsonNodeModel> process = application.process(flow);
             // execute it with one person name
-            System.out.println(application.execute(process, Map.of("name", "Javier")).getWorkflowdata());
+            logger.info(application.execute(process, Map.of("name", "Javier")).getWorkflowdata().toPrettyString());
             // execute it with another person name
-            System.out.println(application.execute(process, Map.of("name", "Alba")).getWorkflowdata());
+            logger.info(application.execute(process, Map.of("name", "Alba")).getWorkflowdata().toPrettyString());
         }
+    }
+
+    private static Map<String, Object> logAge(int age) {
+        logger.info("Age is {}", age);
+        return Collections.emptyMap();
     }
 }
